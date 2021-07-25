579672dd165981df1ca5a576bed6f31016aaae73
shun0305 <shunbbra@icloud.com>
2021-07-25 14:34:06 +0900

    greetingの編集機能

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { greeting: "", web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -25,7 +25,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -38,14 +38,21 @@ class App extends Component {
   runExample = async () => {
     const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    const response = await contract.methods.greet().call();
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    this.setState({ greeting: response });
+  };
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  handleGreetingChange = (e) => {
+    const inputVal = e.target.value;
+    this.setState({ greeting: inputVal });
+  };
+
+  formSubmitHandler = async () => {
+    const { accounts, contract, greeting } = this.state;
+    const updatedGreeting = await contract.methods
+      .setGreeting(greeting)
+      .send({ form: accounts[0] });
   };
 
   render() {
@@ -55,6 +62,17 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Greeter</h1>
+        {this.state.greeting}
+
+        <form>
+          <label>New Greeting</label>
+          <input
+            type="text"
+            value={this.state.greeting}
+            onChange={(e) => this.handleGreetingChange(e)}
+          />
+        </form>
+        <button onClick={this.formSubmitHandler}>submit</button>
       </div>
     );
   }
